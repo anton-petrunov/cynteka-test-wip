5e2aa4e20638bb0b3c0474069856d847e2c4b250
Anton Petrunov <a.n.tone@mail.ru>
2022-11-21 13:58:02 +0300

    Update DataController and DataService

--- src/main/java/io/github/anton_petunov/cynteka_test/service/DataService.java
+++ src/main/java/io/github/anton_petunov/cynteka_test/service/DataService.java
@@ -2,12 +2,16 @@ package io.github.anton_petunov.cynteka_test.service;
 
 import io.github.anton_petunov.cynteka_test.model.Data;
 import io.github.anton_petunov.cynteka_test.repository.InMemoryDataRepository;
-import io.github.anton_petunov.cynteka_test.util.MatchingUtil;
+import io.github.anton_petunov.cynteka_test.to.DataTo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.github.anton_petunov.cynteka_test.util.MatchingUtil.divideAndMatch;
 
 @Service
 @AllArgsConstructor
@@ -28,16 +32,18 @@ public class DataService implements DataServiceInterface {
         return description;
     }
 
-    public List<String> getMatched() {
+    public List<DataTo> getMatched() {
         List<String> first = dataRepository.get().getFirst();
         List<String> second = dataRepository.get().getSecond();
-        List<String> matchingTable = new ArrayList<>();
+        List<DataTo> matchingTable = new ArrayList<>();
         for (String one : first) {
             for (String two : second) {
-                matchingTable.add(MatchingUtil.divideAndMatch(one, two));
+                matchingTable.add(divideAndMatch(one, two));
             }
         }
         System.out.println(matchingTable);
-        return matchingTable;
+        return matchingTable.stream()
+                .sorted(Comparator.comparing(DataTo::getScore).reversed())
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/io/github/anton_petunov/cynteka_test/view/DataController.java
+++ src/main/java/io/github/anton_petunov/cynteka_test/view/DataController.java
@@ -1,6 +1,7 @@
 package io.github.anton_petunov.cynteka_test.view;
 
 import io.github.anton_petunov.cynteka_test.service.DataService;
+import io.github.anton_petunov.cynteka_test.to.DataTo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 
@@ -16,9 +17,9 @@ public class DataController {
 
     @PostConstruct
     public void generate() {
-        List<String> description = dataService.getMatched();
+        List<DataTo> description = dataService.getMatched();
         try (FileWriter fileWriter = new FileWriter("output.txt")) {
-            for (String line : description) {
+            for (DataTo line : description) {
 //                https://ru.stackoverflow.com/a/574105 - using operational system line separator
                 fileWriter.write(line + System.lineSeparator());
             }
